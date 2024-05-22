948946343a902dca28242bf07b8c91e8ec1d3eb7
vladokuskov <vladokuskov@gmail.com>
2024-05-22 20:59:09 +0300

    Refactored docker compose, removed auth middleware from all routes, added comment to .env.example

--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -1,8 +1,5 @@
 import express from "express";
-import { authenticateToken } from "../middleware/authenticateToken.middleware";
 
 const router = express.Router();
-
-router.post("/", authenticateToken);
 
 export default router;
